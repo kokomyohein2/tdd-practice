31481e402e72f40df7e18706b027a1f0ea4a1c2f
Kokomyoheinhtet Unknown <kokomyoheinhtet@ascendcorp.com>
2021-09-14 08:36:48 +0700

    added isbn 13 digit cases

--- src/io/petelleave/tddpractice/ValidateISBN.java
+++ src/io/petelleave/tddpractice/ValidateISBN.java
@@ -3,21 +3,32 @@ package io.petelleave.tddpractice;
 public class ValidateISBN {
 
     public boolean checkISBN(String isbn) {
-        if (isbn.length() != 10) {
+        if (isbn.length() == 13) {
+            int total = 0;
+            for (int i = 0; i < 13; i++) {
+                if (i % 2 == 0) {
+                    total += Character.getNumericValue(isbn.charAt(i));
+                } else {
+                    total += Character.getNumericValue(isbn.charAt(i)) * 3;
+                }
+            }
+            return total % 10 == 0;
+        } else if (isbn.length() == 10) {
+            int total = 0;
+            for (int i = 0; i < 10; i++) {
+                if (!Character.isDigit(isbn.charAt(i))) {
+                    if (i == 9 && isbn.charAt(i) == 'X') {
+                        total += 10;
+                    } else {
+                        throw new NumberFormatException("Must be digits only");
+                    }
+                } else {
+                    total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
+                }
+            }
+            return total % 11 == 0;
+        } else {
             throw new NumberFormatException("Must be 10 digits");
         }
-        int total = 0;
-        for (int i = 0; i < 10; i++) {
-            if (!Character.isDigit(isbn.charAt(i))) {
-                if (i == 9 && isbn.charAt(i) == 'X') {
-                    total += 10;
-                } else {
-                    throw new NumberFormatException("Must be digits only");
-                }
-            } else {
-                total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
-            }
-        }
-        return total % 11 == 0;
     }
 }
--- src/io/petelleave/tddpractice/ValidateISBNTest.java
+++ src/io/petelleave/tddpractice/ValidateISBNTest.java
@@ -7,23 +7,40 @@ import static org.junit.Assert.*;
 public class ValidateISBNTest {
 
     @Test
-    public void checkValidISBN() {
+    public void checkValid10DigitISBN() {
         ValidateISBN validateISBN = new ValidateISBN();
         boolean result = validateISBN.checkISBN("1413304540");
         assertTrue("first", result);
     }
 
     @Test
-    public void ISBNNumbersEndingInAnXAreValid() {
+    public void checkAValid13DigitISBN() {
+        ValidateISBN validateISBN = new ValidateISBN();
+        boolean result = validateISBN.checkISBN("9781853260087");
+        assertTrue("first value", result);
+         result = validateISBN.checkISBN("9781853267338");
+        assertTrue("second value", result);
+
+    }
+
+    @Test
+    public void TenDigitISBNNumbersEndingInAnXAreValid() {
         ValidateISBN validateISBN = new ValidateISBN();
         boolean result = validateISBN.checkISBN("012000030X");
         assertTrue(result);
     }
 
     @Test
-    public void checkInvalidISBN() {
+    public void checkInvalid10DigitISBN() {
         ValidateISBN validateISBN = new ValidateISBN();
         boolean result = validateISBN.checkISBN("1413304543");
+        assertFalse(result);
+    }
+
+    @Test
+    public void checkInvalid13DigitISBN() {
+        ValidateISBN validateISBN = new ValidateISBN();
+        boolean result = validateISBN.checkISBN("9781853267335");
         assertFalse(result);
     }
 
